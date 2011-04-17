572dc847404cf95b13362cdd3ed0b8419787652c
Antonio Barra <masermoon@gmail.com>
2011-04-16 18:09:45 -0700

    Edited lib/GoogleURL.rb via GitHub

--- lib/GoogleURL.rb
+++ lib/GoogleURL.rb
@@ -13,38 +13,38 @@ module GoogleURL
    
    class Shorting
       def initialize(long_url)
-	     if !(GoogleURL.c(long_url))
-		   p "#{long_url}: Is not a valid link."
-		   exit
-		 end
-	     par = {#'user' => 'youruser@google.com',
-                 'url' => long_url,
-	             #'auth_token' => '78925814685'
-                 #'key' => "your API key"
-	           }
-	     res = Net::HTTP.post_form(URI.parse("http://goo.gl/api/url"), par)
-         return_link = JSON.parse(res.body)
-		 puts return_link['short_url']
-	  end
+	   if !(GoogleURL.c(long_url))
+              p "#{long_url}: Is not a valid link."
+              exit
+           end
+	   par = { #'user' => 'youruser@google.com',
+                      'url' => long_url,
+	              #'auth_token' => '78925814685'
+                      #'key' => "your API key" }
+	   res = Net::HTTP.post_form(URI.parse("http://goo.gl/api/url"), par)
+           return_link = JSON.parse(res.body)
+           puts return_link['short_url']
+      end
    end
    
    class Expand
       def initialize(short_url)
-	     if !(GoogleURL.c(short_url))
-		   p "#{short_url}: Is not a valid link."
-		   exit
-		 end
-	     url = "https://www.googleapis.com/urlshortener/v1/url?"
-		 uri = URI.parse(url + "shortUrl=" + short_url)
-		 # Security check SSL do
-         http = Net::HTTP.new(uri.host, uri.port)
-         http.use_ssl = true
-         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		 # end
-		 request = Net::HTTP::Get.new(uri.request_uri)
-         response = http.request(request)
-		 return_link = JSON.parse(response.body)
-		 puts return_link['longUrl']
-	  end
+	   if !(GoogleURL.c(short_url))
+              p "#{short_url}: Is not a valid link."
+	      exit
+           end
+	   url = "https://www.googleapis.com/urlshortener/v1/url?"
+           uri = URI.parse(url + "shortUrl=" + short_url)
+	   # Security check SSL do
+           http = Net::HTTP.new(uri.host, uri.port)
+           http.use_ssl = true
+           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+           # end
+           request = Net::HTTP::Get.new(uri.request_uri)
+           response = http.request(request)
+           return_link = JSON.parse(response.body)
+           puts return_link['longUrl']
+      end
    end
+
 end
